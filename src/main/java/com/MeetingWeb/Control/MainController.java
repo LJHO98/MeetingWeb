44bd0ec1e1c4f0682c27ee2e84f8225187ce6536
부계정 <ehdgoanf714@naver.com>
2024-11-01 09:44:30 +0900

    test

--- src/main/java/com/MeetingWeb/Control/MainController.java
+++ src/main/java/com/MeetingWeb/Control/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-
-
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
 }
