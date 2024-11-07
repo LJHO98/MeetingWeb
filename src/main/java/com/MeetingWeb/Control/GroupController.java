526367e52b0727e6bb9d3c7620baa613f79a854a
Seohyeongjun <seo.hyeongjun@gmail.com>
2024-11-07 17:47:27 +0900

    1234

--- src/main/java/com/MeetingWeb/Control/GroupController.java
+++ src/main/java/com/MeetingWeb/Control/GroupController.java
@@ -5,15 +5,12 @@ import com.MeetingWeb.Entity.User;
 import com.MeetingWeb.Service.GroupService;
 import com.MeetingWeb.Service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
